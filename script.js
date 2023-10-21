3ad04d32c2e430e5208994cc8a361188e8e1d1cd
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-10-22 01:54:30 +0200

    bitcoin-kurs
    added init function and some html elements

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const API_KEY =  'M2LK7TBTRG91HN1W';
 
+
+function init() {
+    loadCourse();
+}
+
+
 async function loadCourse() {
     let url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CHF&apikey=${API_KEY}`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
-    console.log(responseAsJson);
+    let currentCourse = Math.round(responseAsJson['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    console.log(currentCourse);
+
+    document.getElementById('course').innerHTML = `<b>${currentCourse} CHF</b>`;
 }
